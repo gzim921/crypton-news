b53fe8664301bede60bbe3aa59bdc7c50e338240
gzim921 <gzimiseni7@gmail.com>
2021-07-21 23:57:39 +0200

    Refactoring the price page to post method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,18 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
-
-    # Grab Prices Data
-    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX,SHIB&tsyms=EUR'
-    @prices_uri = URI(@prices_url)
-    @prices_response = Net::HTTP.get(@prices_uri)
-    @prices = JSON.parse(@prices_response)
-
   end
 
   def prices
+    require 'net/http'
+    require 'json'
+    @symbol = params[:sym]
+    @symbol = @symbol.upcase
+
+    # Grab Prices Data
+    @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=EUR'
+    @quote_uri = URI(@quote_url)
+    @quote_response = Net::HTTP.get(@quote_uri)
+    @quote = JSON.parse(@quote_response)
   end
 end
